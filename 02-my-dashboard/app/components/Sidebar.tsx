d523ccc53e0b87a134051dc03128d3c0c09c7880
Nabel Hernandez <nabelitahm@gmail.com>
2026-01-27 22:13:57 +0100

    feat: initialize my-dashboard project with Next.js, TypeScript, and Tailwind CSS
    - Added package.json with dependencies for Next.js, React, and Tailwind CSS.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Added SVG assets for icons and branding in the public directory.
    - Configured TypeScript with tsconfig.json for strict type checking and module resolution.

---
+++ 02-my-dashboard/app/components/Sidebar.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image"
+import { IoBrowsersOutline, IoCalculator, IoLogoReact } from "react-icons/io5"
+import { SidebarMenuItem } from "./SidebarMenuItem"
+
+const menuItems = [
+    {
+        path: '/dashboard/main',
+        icon: <IoBrowsersOutline size={40}/>,
+        title: 'Dashboard',
+        subtitle: 'Visualización',
+    },
+    {
+        path: '/dashboard/counter',
+        icon: <IoCalculator size={40}/>,
+        title: 'Counter',
+        subtitle: 'Contador Client Side',
+    },
+]
+
+export const Sidebar = () => {
+    return (
+        <div id="menu"
+            style={{ width: '400px' }}
+            className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen overflow-y-scroll">
+            <div id="logo" className="my-4 px-6">
+                <h1 className="flex items-center text-lg md:text-2xl font-bold text-white">
+                    <IoLogoReact />
+                    <span>Dash</span>
+                    <span className="text-blue-500">8</span>.</h1>
+                <p className="text-slate-500 text-sm">Manage your actions and activities</p>
+            </div>
+            <div id="profile" className="px-6 py-10">
+                <p className="text-slate-500">Welcome back,</p>
+                <a href="#" className="inline-flex space-x-2 items-center">
+                    <span>
+                        <Image
+                            className="rounded-full w-8 h-8"
+                            src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=128&q=80"
+                            width={50}
+                            height={50}
+                            alt="Picture of the author"
+                        />
+                    </span>
+                    <span className="text-sm md:text-base font-bold">
+                        Nabel Hernandez
+                    </span>
+                </a>
+            </div>
+            <div id="nav" className="w-full px-6">
+                {
+                    menuItems.map( item =>
+                        <SidebarMenuItem
+                            key={item.path}
+                            {...item}
+                        />
+                    )
+                }
+            </div>
+        </div>
+    )
+}
---
+++ 02-my-dashboard/app/components/shopping-cart/components/CartCounter.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { useState } from "react";
+
+interface Props {
+  value?: number
+}
+
+export const CartCounter = ({ value = 0 }: Props) => {
+  const [counter, setCounter] = useState( value );
+
+  return (
+    <>
+      <span className="text-7xl">{counter}</span>
+
+      <div className="flex">
+        <button
+          onClick={() => setCounter(counter + 1)}
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-25 mr-2">
+          +1
+        </button>
+        <button
+          onClick={() => setCounter(counter - 1)}
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-25 mr-2">
+          -1
+        </button>
+      </div>
+    </>
+  )
+} 
